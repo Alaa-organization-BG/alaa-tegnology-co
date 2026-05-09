e236e803840b45e1e60911ef286275c155979fe7
Alaa Al-Hamadani <hamdanialaa@yahoo.com>
2026-05-09 03:40:49 +0300

    feat(firebase): integrate firebase sdk and connect beta form to firestore

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+// Firebase configuration for Alaa Tegnology CO
+const firebaseConfig = {
+  apiKey: "AIzaSyCXAo_FyObuihBPJbf_GXS-owIEDRKFTZs",
+  authDomain: "alaa-tegnology-co-fire.firebaseapp.com",
+  projectId: "alaa-tegnology-co-fire",
+  storageBucket: "alaa-tegnology-co-fire.firebasestorage.app",
+  messagingSenderId: "297605741592",
+  appId: "1:297605741592:web:76ef2bd1c123158272576d",
+  measurementId: "G-JHVJ0EP5DS"
+};
+
+// Exporting for use in other scripts
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = firebaseConfig;
+}
